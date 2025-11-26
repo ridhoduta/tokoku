e2f62d76747cbeef2ed0582d43ef6434e55cbe8d
ridhoduta <ridhodutay@gmail.com>
2025-11-26 20:11:56 +0700

    push deploy

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
+// Serve React for root
 Route::get('/', function () {
-    return view('welcome');
+    return file_get_contents(public_path('react/index.html'));
 });
+
+// Catch-all untuk SPA, tapi jangan ganggu folder react/
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('react/index.html'));
+})->where('any', '^(?!react/).*$');
