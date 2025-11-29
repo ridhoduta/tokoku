77ae781bbe7fc0a77e66be7ed26dd3fa34c63e81
ridhoduta <ridhodutay@gmail.com>
2025-11-29 19:19:43 +0700

    update route web

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-// Serve React for root
+// Auto redirect pertama kali ke /login (React)
 Route::get('/', function () {
-    return file_get_contents(public_path('react/index.html'));
+    return redirect('/login');
 });
 
-// Catch-all untuk SPA, tapi jangan ganggu folder react/
+// Catch-all route untuk React SPA
 Route::get('/{any}', function () {
     return file_get_contents(public_path('react/index.html'));
 })->where('any', '^(?!react/).*$');
+
+Route::get('/test-env', function () {
+    return env('FIREBASE_PROJECT_ID');
+});
